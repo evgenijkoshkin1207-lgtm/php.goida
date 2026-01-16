6b74dd87e728885b5e552c813a3fabbb65d52348
ol1s <evgenijkoshkin1207@gmail.com>
2026-01-16 13:03:21 +0300

    lab3 1

--- lob3/index.php
+++ lob3/index.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head> 
+<body>
+    <form action="index.php" method="get">
+        <input type="text" name="вСелоМолочность" id="">
+        <button type="submit">goyda</button>
+    </form>
+</body>
+</html>
+
 <?php
-    echo "hi Gitler!<RB>";
-?>
+    if (isset($_GET['вСелоМолочность'])){
+        $massiv_slov = explode(' ', $_GET['вСелоМолочность']);
+        chelofek($massiv_slov);
+        echo implode(' ', $massiv_slov);
+    }
+    
+    function chelofek(&$massiv_slov){
+        for($i = 0; $i < count($massiv_slov); $i++){
+            if(($i % 2) > 0 ){
+                $massiv_slov[$i] = strtoupper ($massiv_slov[$i]);
+            }
+        }
+    }
+
+    ?>
